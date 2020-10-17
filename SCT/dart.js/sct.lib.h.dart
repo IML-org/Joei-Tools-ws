df1a2261a3b68b103683c39a7a8d3959cdfd631c
HALKA9000-NIIP <csta19097@g.nihon-u.ac.jp>
2020-10-17 11:46:02 +0900

    Update sct.lib.h.dart

--- SCT/dart.js/sct.lib.h.dart
+++ SCT/dart.js/sct.lib.h.dart
@@ -1,32 +1,51 @@
-void main(List<String> args) {
-  var testS =new Sou_Cal(1,2,3,4,5);
-  print(testS.nire);
-}
-class Sou_Cal {
-  num sNen;
-  num sJi;
-  num you;
-  num nire;
-  num taku;
-  num myou;
+library "sct_dart";
+
+class EarthlyCal{
+  String system;
+
+  num year;
+  num month;
+  num week;
+  num day;
+  num hour;
+  num minuits;
+  num seconds;
+  num milisec;
   num timestamp;
-  Sou_Cal(this.sNen,this.you,this.taku,this.nire,this.myou);
-  void ts(timestamp){
+
+  Map<Map<dynamic>> data;
+  data.sou={months:12,daysPerMons:[,,,,],sysNames:{jp:["","","","you","nire","taku","myou"],en:["","","","you","nire","taku","myou"]},secVal:};
+  data.ear;
+
+  Cal_Sys(this.system);
+
+  void datetime(num year,num month,num day,num hour,num minuits,num seconds){
+    this.year,this.you,this.taku,this.nire,this.myou;
+  };
+
+  void ts(num timestamp){
     this.timestamp=timestamp;
     this.calcDate();
   }
+
   num toTS(){
+    this.calcTS();
+    return this.timestamp;
 
   }
+
   void calcTS(){
 
   }
+
   void calcDate(){
 
   }
-  String format(String format){}
+  String format(String formatSettle){
+    /* abcdefg 
+    day.lcs
+    */
+  }
 }
-class Ear_Cal{
 
-}
 
